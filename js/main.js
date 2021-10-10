2029c6d4e98646e8e78cf00cc6a2960d048005ce
KION <kion1491@gmail.com>
2021-10-10 17:41:37 +0900

    directory root modify

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const imgLength = 88;
 
 for (let i = 0; i <= imgLength-1; i++){
     let htmlTag = `<figure>
-        <img src="/images/img_${i+1}.JPG" alt="">
+        <img src="./images/img_${i+1}.JPG" alt="">
     </figure>`;
 
     columns.innerHTML += htmlTag;
