4b37a9c14e70ef1fc2cc12eff4994c817e3fdc90
KION <kion1491@gmail.com>
2021-10-10 16:55:55 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const columns = document.getElementById('columns');
+const imgLength = 88;
+
+for (let i = 0; i <= imgLength-1; i++){
+    let htmlTag = `<figure>
+        <img src="/images/img_${i+1}.JPG" alt="">
+    </figure>`;
+
+    columns.innerHTML += htmlTag;
+}
