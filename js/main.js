66e4d7b907cb5e9b869fa795de12ab4e0ea9fcb4
KION <kion1491@gmail.com>
2021-10-11 00:58:55 +0900

    dimmed popup function added
    1. dimmed 팝업으로 이미지 확대기능
    2. x버튼 누르면 레이어팝업 닫힘

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 const columns = document.getElementById('columns');
+const mainSection = document.getElementById('main-section');
 const imgLength = 88;
+
 
 for (let i = 0; i <= imgLength-1; i++){
     let htmlTag = `<figure>
-        <img src="./images/img_${i+1}.JPG" alt="">
+        <img class="image" src="./images/img_${i+1}.JPG" alt="">
     </figure>`;
 
     columns.innerHTML += htmlTag;
 }
+
+// dimmed 관련 함수영역
+let dimmedImg;
+
+function dimmedFunc(src){
+    let dimmedImg = document.getElementById('dimmedImg');
+    dimmedImg.src = src;
+}
+
+document.addEventListener("click", function(e){
+    let target = e.target;
+    let dimmedBox = document.getElementById('dimmedBox');
+
+    // 이미지 확대 기능 (dimmed box 열기)
+    if(target.classList == "image"){
+        dimmedFunc(e.target.src);
+        dimmedBox.style.display = 'flex';
+    }
+
+    // 확대된 이미지 닫기 (dimmed box 닫기)
+    if(target.id == 'dimmedCloseBtn'){
+        dimmedBox.style.display = 'none';
+    }
+
+})
+
